60a966ed6a15c4b7dfa8b7dc5866e95447cc984c
noob247365 <noob247365@gmail.com>
2018-09-30 22:42:54 -0400

    Initial commit with test code
    This time I properly configured my Git credentials

---
+++ Helpers.cs
@@ -0,0 +1,28 @@
+using System;
+ using System.IO;
+
+ using SkiaSharp;
+
+ namespace ThumbnailCreator
+ {
+     public static class Helpers
+     {
+         #region Image IO
+
+         public static void SaveToFile(this SKImage image, string fileName,
+             SKEncodedImageFormat format = SKEncodedImageFormat.Png, int quality = 100)
+         {
+             // Ensure the output path exists
+             string fullPath = Path.GetFullPath(fileName);
+             Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+
+             // Actually save to the file
+             using (var stream = File.Create(fullPath))
+             using (var data = image.Encode(format, quality))
+                 data.SaveTo(stream);
+         }
+
+         #endregion
+     }
+ }
+ 
